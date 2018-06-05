a4fb248b1c0e9f3d452421eef3586ad58fdf462c
Luisa Bolado <mboladop@hotmail.com>
2018-06-05 09:05:47 +0000

    Initial Commit

---
+++ tweet-web.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+def get_index():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
